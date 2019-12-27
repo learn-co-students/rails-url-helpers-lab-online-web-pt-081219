b89b6b7048be654329302d8655278c7948a9c833
wifelette <LeahSilber@gmail.com>
2019-12-26 16:45:32 -0800

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,23 @@
+# frozen_string_literal: true
+
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: %i[show activate]
+
   def index
     @students = Student.all
   end
 
-  def show
+  def show; end
+
+  def activate
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
 
-    def set_student
-      @student = Student.find(params[:id])
-    end
+  def set_student
+    @student = Student.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :students, only: %i[index show]
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
