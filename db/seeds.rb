222c1c144269252443206de95795a44dfa94b39d
Dylan Crawshaw <dcrawsh@gmail.com>
2019-12-31 13:19:16 -0800

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Student.create(first_name: 'Dylan', last_name: 'Crawshaw')
+Student.create(first_name: 'Liam', last_name: 'Crawshaw', active: true)
